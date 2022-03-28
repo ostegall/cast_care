a3444038dc0df098a95d3a5bd1834a8ae02f36bb
Owen Stegall <ostegall@live.unc.edu>
2022-03-28 09:53:37 -0400

    Update Cast Solution Structure

--- App.js
+++ App.js
@@ -661,7 +661,7 @@ function SomethingInCast({navigation}) { //Solutions for user that has something
           </Pressable>
           </View>
         </View>
-      )
+    )
 }
 
 function CastBreaking({navigation}) { //Solutions for user that has a breaking/broken cast
